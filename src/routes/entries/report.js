0d06bef18ee2feb5a716586a4f87b89dff75ae24
Gerson E. Aguirre <hola@gersonaguirre.me>
2021-01-15 14:29:05 -0600

    OC-174: Avances en reporte diario mayor

--- src/routes/entries/report.js
+++ src/routes/entries/report.js
@@ -4,10 +4,9 @@ const { startOfMonth, endOfMonth, format } = require("date-fns");
 const router = express.Router();
 
 router.get("/diario-mayor", async (req, res) => {
-  const check = checkRequired(
-    req.query,
-    [{ name: "date", type: "date", optional: false }],
-  );
+  const check = checkRequired(req.query, [
+    { name: "date", type: "date", optional: false },
+  ]);
   if (!check.success) {
     return res.status(400).json({ message: check.message });
   }
@@ -28,8 +27,8 @@ router.get("/diario-mayor", async (req, res) => {
       .where("d.company = :company", { company: req.user.cid })
       .andWhere("e.date >= :startDate", { startDate: startOfMonth(date) })
       .andWhere("e.date <= :endDate", { endDate: endOfMonth(date) })
-      .leftJoinAndSelect('d.accountingEntry', 'e')
-      .leftJoinAndSelect('d.accountingCatalog', 'c')
+      .leftJoinAndSelect("d.accountingEntry", "e")
+      .leftJoinAndSelect("d.accountingCatalog", "c")
       .getMany();
 
     // obtiene los detalles de la partida anteriores al rango seleccionado
@@ -38,53 +37,67 @@ router.get("/diario-mayor", async (req, res) => {
       .createQueryBuilder("d")
       .where("d.company = :company", { company: req.user.cid })
       .andWhere("e.date < :date", { date: startOfMonth(date) })
-      .leftJoinAndSelect('d.accountingEntry', 'e')
-      .leftJoinAndSelect('d.accountingCatalog', 'c')
+      .leftJoinAndSelect("d.accountingEntry", "e")
+      .leftJoinAndSelect("d.accountingCatalog", "c")
       .getMany();
 
     // define el listado de cuentas contables afectadas en el periodo seleccionado
-    const affectedCatalog = [...new Set(rangeDetails.map(d => d.accountingCatalog.code))]
+    const affectedCatalog = [
+      ...new Set(rangeDetails.map((d) => d.accountingCatalog.code)),
+    ];
 
-    let accounts = []
+    let accounts = [];
     // obtiene el saldo inicial por cuenta
-    accounts = affectedCatalog.map(c => {
-      const account = catalog.find(ct => ct.code == c)
-      const abono = previousDetails.filter(d => d.accountingCatalog.code == c).reduce((a, b) => a + (b.abono ? b.abono : 0), 0)
-      const cargo = previousDetails.filter(d => d.accountingCatalog.code == c).reduce((a, b) => a + (b.cargo ? b.cargo : 0), 0)
-      const applicable = rangeDetails.filter(d => d.accountingCatalog.code == c)
-      const movements = []
+    accounts = affectedCatalog.map((c) => {
+      const account = catalog.find((ct) => ct.code == c);
+      const abono = previousDetails
+        .filter((d) => d.accountingCatalog.code == c)
+        .reduce((a, b) => a + (b.abono ? b.abono : 0), 0);
+      const cargo = previousDetails
+        .filter((d) => d.accountingCatalog.code == c)
+        .reduce((a, b) => a + (b.cargo ? b.cargo : 0), 0);
+      const applicable = rangeDetails.filter(
+        (d) => d.accountingCatalog.code == c
+      );
+      const movements = [];
       for (let item of applicable) {
-        const found = movements.find(m => m.date == item.accountingEntry.date)
+        const found = movements.find(
+          (m) => m.date == item.accountingEntry.date
+        );
         if (found) {
-          console.log('entra')
-          item.cargo = found.cargo + (item.cargo ? item.cargo : 0)
-          item.abono = found.abono + (item.abono ? item.abono : 0)
+          console.log("entra");
+          item.cargo = found.cargo + (item.cargo ? item.cargo : 0);
+          item.abono = found.abono + (item.abono ? item.abono : 0);
         } else {
           movements.push({
             date: item.accountingEntry.date,
             cargo: item.cargo ? item.cargo : 0,
-            abono: item.abono ? item.abono : 0
-          })
+            abono: item.abono ? item.abono : 0,
+            balance: 0,
+          });
         }
       }
       return {
         code: c,
         name: account.name,
-        initialBalance: (account.isAcreedora ? abono - cargo : cargo - abono).toFixed(2),
+        initialBalance: (account.isAcreedora
+          ? abono - cargo
+          : cargo - abono
+        ).toFixed(2),
         movements: movements.sort((a, b) => {
-          if (new Date(a.date) > new Date(b.date)) return 1
-          if (new Date(a.date) < new Date(b.date)) return -1
-          return 0
+          if (new Date(a.date) > new Date(b.date)) return 1;
+          if (new Date(a.date) < new Date(b.date)) return -1;
+          return 0;
         }),
         totalAbono: 0,
         totalCargo: 0,
         currentBalance: 0,
-      }
-    })
+      };
+    });
 
     return res.json({ accounts });
   } catch (error) {
-    console.log(error)
+    console.log(error);
     return res
       .status(500)
       .json({ message: "Error al obtener el listado de tipos de partida." });
