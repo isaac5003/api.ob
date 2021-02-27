0801163a509bb1fe95b1b65218a4f9f7e18f1a88
Bryanavalos-dev <bryanavalos91@gmail.com>
2021-02-26 19:31:04 -0600

    OB-83: ADVANCES  -Agregando el campo cashPayment a la BD, y al metodo POST y PUT

--- src/routes/invoices/paymentCondition.js
+++ src/routes/invoices/paymentCondition.js
@@ -1,17 +1,17 @@
-const express = require("express");
-const { checkRequired, foundRelations, addLog } = require("../../tools");
+const express = require('express');
+const { checkRequired, foundRelations, addLog } = require('../../tools');
 const router = express.Router();
 
-router.get("/", async (req, res) => {
+router.get('/', async (req, res) => {
   const check = checkRequired(
     req.query,
     [
-      { name: "limit", type: "integer", optional: true },
-      { name: "page", type: "integer", optional: true },
-      { name: "active", type: "boolean", optional: true },
-      { name: "search", type: "string", optional: true },
+      { name: 'limit', type: 'integer', optional: true },
+      { name: 'page', type: 'integer', optional: true },
+      { name: 'active', type: 'boolean', optional: true },
+      { name: 'search', type: 'string', optional: true },
     ],
-    true
+    true,
   );
   if (!check.success) {
     return res.status(400).json({ message: check.message });
@@ -22,25 +22,25 @@ router.get("/", async (req, res) => {
     const { limit, page, active, search } = req.query;
 
     let query = req.conn
-      .getRepository("InvoicesPaymentsCondition")
-      .createQueryBuilder("pc")
-      .where("pc.company = :company", { company: cid })
-      .select("COUNT(pc.id)", "count");
+      .getRepository('InvoicesPaymentsCondition')
+      .createQueryBuilder('pc')
+      .where('pc.company = :company', { company: cid })
+      .select('COUNT(pc.id)', 'count');
 
     if (active != null) {
-      query = query.andWhere("pc.active = :active", {
-        active: active == "true",
+      query = query.andWhere('pc.active = :active', {
+        active: active == 'true',
       });
     }
 
     let { count } = await query.getRawOne();
 
     let paymentConditions = req.conn
-      .getRepository("InvoicesPaymentsCondition")
-      .createQueryBuilder("pc")
-      .select(["pc.id", "pc.name", "pc.active"])
-      .where("pc.company = :company", { company: cid })
-      .orderBy("pc.createdAt", "DESC");
+      .getRepository('InvoicesPaymentsCondition')
+      .createQueryBuilder('pc')
+      .select(['pc.id', 'pc.name', 'pc.active'])
+      .where('pc.company = :company', { company: cid })
+      .orderBy('pc.createdAt', 'DESC');
 
     let index = 1;
     if (search == null) {
@@ -51,59 +51,59 @@ router.get("/", async (req, res) => {
     }
 
     if (active != null) {
-      paymentConditions = paymentConditions.andWhere("s.active = :active", {
-        active: active == "true",
+      paymentConditions = paymentConditions.andWhere('s.active = :active', {
+        active: active == 'true',
       });
     }
     paymentConditions = await paymentConditions.getMany();
 
     if (search != null) {
-      paymentConditions = paymentConditions.filter((s) =>
-        s.name.toLowerCase().includes(search)
-      );
+      paymentConditions = paymentConditions.filter(s => s.name.toLowerCase().includes(search));
       count = paymentConditions.length;
     }
 
     return res.json({
       count,
-      paymentConditions: paymentConditions.map((s) => {
+      paymentConditions: paymentConditions.map(s => {
         return { index: index++, ...s };
       }),
     });
   } catch (error) {
     console.error(error);
-    return res
-      .status(500)
-      .json({ message: "Error al obtener el listado de condiciones de pago." });
+    return res.status(500).json({ message: 'Error al obtener el listado de condiciones de pago.' });
   }
 });
 
-router.post("/", async (req, res) => {
+router.post('/', async (req, res) => {
   // Verifica los campos requeridos
-  const check = checkRequired(req.body, ["name"]);
+  const check = checkRequired(req.body, [
+    { name: 'name', type: 'boolean', optional: false },
+    { name: 'cashPayment', type: 'boolean', optional: false },
+  ]);
   if (!check.success) {
     return res.status(400).json({ message: check.message });
   }
 
   // Obtiene los campos requeridos
-  const { name } = req.body;
+  const { name, cashPayment } = req.body;
 
   // Inserta la condicion de pago
   try {
     const paymentCondition = await req.conn
       .createQueryBuilder()
       .insert()
-      .into("InvoicesPaymentsCondition")
+      .into('InvoicesPaymentsCondition')
       .values({
         name,
+        cashPayment,
         company: req.user.cid,
       })
       .execute();
 
     const user = await req.conn
-      .getRepository("User")
-      .createQueryBuilder("u")
-      .where("u.id = :id", { id: req.user.uid })
+      .getRepository('User')
+      .createQueryBuilder('u')
+      .where('u.id = :id', { id: req.user.uid })
       .getOne();
 
     await addLog(
@@ -111,46 +111,48 @@ router.post("/", async (req, res) => {
       req.moduleName,
       `${user.names} ${user.lastnames}`,
       user.id,
-      `Se ha creado la condicion de pago: ${name}`
+      `Se ha creado la condicion de pago: ${name}`,
     );
 
     // On success
     return res.json({
-      message: "La condicion de pago se ha creado correctamente.",
+      message: 'La condicion de pago se ha creado correctamente.',
       id: paymentCondition.raw[0].id,
     });
   } catch (error) {
     // On errror
     return res.status(400).json({
-      message:
-        "Error al guardar la nueva condicion de pago, contacta con tu administrador.",
+      message: 'Error al guardar la nueva condicion de pago, contacta con tu administrador.',
     });
   }
 });
 
-router.put("/:id", async (req, res) => {
+router.put('/:id', async (req, res) => {
   // Verifica los campos requeridos
-  const check = checkRequired(req.body, ["name"]);
+  const check = checkRequired(req.body, [
+    { name: 'name', type: 'boolean', optional: false },
+    { name: 'cashPayment', type: 'boolean', optional: false },
+  ]);
   if (!check.success) {
     return res.status(400).json({ message: check.message });
   }
 
   // Obtiene los campos requeridos
-  const { name } = req.body;
+  const { name, cashPayment } = req.body;
 
   // Actualiza la condicion de pago
   try {
     await req.conn
       .createQueryBuilder()
-      .update("InvoicesPaymentsCondition")
-      .set({ name })
-      .where("id = :id", { id: req.params.id })
+      .update('InvoicesPaymentsCondition')
+      .set({ name, cashPayment })
+      .where('id = :id', { id: req.params.id })
       .execute();
 
     const user = await req.conn
-      .getRepository("User")
-      .createQueryBuilder("u")
-      .where("u.id = :id", { id: req.user.uid })
+      .getRepository('User')
+      .createQueryBuilder('u')
+      .where('u.id = :id', { id: req.user.uid })
       .getOne();
 
     await addLog(
@@ -158,25 +160,24 @@ router.put("/:id", async (req, res) => {
       req.moduleName,
       `${user.names} ${user.lastnames}`,
       user.id,
-      `Se ha editado la condicion de pago: ${name}`
+      `Se ha editado la condicion de pago: ${name}`,
     );
 
     // On success
     return res.json({
-      message: "La condicion de pago ha sido actualizada correctamente.",
+      message: 'La condicion de pago ha sido actualizada correctamente.',
     });
   } catch (error) {
     // On errror
     return res.status(400).json({
-      message:
-        "Error al actualizar la condicion de pago, contacta con tu administrador.",
+      message: 'Error al actualizar la condicion de pago, contacta con tu administrador.',
     });
   }
 });
 
-router.put("/status/:id", async (req, res) => {
+router.put('/status/:id', async (req, res) => {
   // Check required field
-  const check = checkRequired(req.body, ["status"]);
+  const check = checkRequired(req.body, ['status']);
   if (!check.success) {
     return res.status(400).json({ message: check.message });
   }
@@ -186,17 +187,15 @@ router.put("/status/:id", async (req, res) => {
 
   // Get paymentCondition
   const paymentCondition = await req.conn
-    .getRepository("InvoicesPaymentsCondition")
-    .createQueryBuilder("pc")
-    .where("pc.company = :company", { company: req.user.cid })
-    .andWhere("pc.id = :id", { id: req.params.id })
+    .getRepository('InvoicesPaymentsCondition')
+    .createQueryBuilder('pc')
+    .where('pc.company = :company', { company: req.user.cid })
+    .andWhere('pc.id = :id', { id: req.params.id })
     .getOne();
 
   // If no exist
   if (!paymentCondition) {
-    return res
-      .status(400)
-      .json({ message: "La condicion de pago seleccionada no existe." });
+    return res.status(400).json({ message: 'La condicion de pago seleccionada no existe.' });
   }
 
   // If paymentCondition exist updates it
@@ -204,16 +203,16 @@ router.put("/status/:id", async (req, res) => {
     // return success
     await req.conn
       .createQueryBuilder()
-      .update("InvoicesPaymentsCondition")
+      .update('InvoicesPaymentsCondition')
       .set({ active: status })
-      .where("company = :company", { company: req.user.cid })
-      .where("id = :id", { id: req.params.id })
+      .where('company = :company', { company: req.user.cid })
+      .where('id = :id', { id: req.params.id })
       .execute();
 
     const user = await req.conn
-      .getRepository("User")
-      .createQueryBuilder("u")
-      .where("u.id = :id", { id: req.user.uid })
+      .getRepository('User')
+      .createQueryBuilder('u')
+      .where('u.id = :id', { id: req.user.uid })
       .getOne();
 
     await addLog(
@@ -221,36 +220,32 @@ router.put("/status/:id", async (req, res) => {
       req.moduleName,
       `${user.names} ${user.lastnames}`,
       user.id,
-      `Se cambio el estado de la condicion de pago: ${paymentCondition.name
-      } a ${status ? "ACTIVO" : "INACTIVO"}.`
+      `Se cambio el estado de la condicion de pago: ${paymentCondition.name} a ${status ? 'ACTIVO' : 'INACTIVO'}.`,
     );
 
     return res.json({
-      message: "La condicion de pago ha sido actualizada correctamente.",
+      message: 'La condicion de pago ha sido actualizada correctamente.',
     });
   } catch (error) {
     // return error
     return res.status(500).json({
-      message:
-        "Error al actualizar la condicion de pago. Contacta con tu administrador.",
+      message: 'Error al actualizar la condicion de pago. Contacta con tu administrador.',
     });
   }
 });
 
-router.delete("/:id", async (req, res) => {
+router.delete('/:id', async (req, res) => {
   // Get the paymentCondition
   const paymentCondition = await req.conn
-    .getRepository("InvoicesPaymentsCondition")
-    .createQueryBuilder("pc")
-    .where("pc.company = :company", { company: req.user.cid })
-    .andWhere("pc.id = :id", { id: req.params.id })
+    .getRepository('InvoicesPaymentsCondition')
+    .createQueryBuilder('pc')
+    .where('pc.company = :company', { company: req.user.cid })
+    .andWhere('pc.id = :id', { id: req.params.id })
     .getOne();
 
   // If no paymentCondition exist
   if (!paymentCondition) {
-    return res
-      .status(400)
-      .json({ message: "La condicion de pago ingresada no existe" });
+    return res.status(400).json({ message: 'La condicion de pago ingresada no existe' });
   }
 
   // If paymentCondition exist
@@ -276,15 +271,15 @@ router.delete("/:id", async (req, res) => {
     await req.conn
       .createQueryBuilder()
       .delete()
-      .from("InvoicesPaymentsCondition")
-      .where("id = :id", { id: req.params.id })
-      .andWhere("company = :company", { company: req.user.cid })
+      .from('InvoicesPaymentsCondition')
+      .where('id = :id', { id: req.params.id })
+      .andWhere('company = :company', { company: req.user.cid })
       .execute();
 
     const user = await req.conn
-      .getRepository("User")
-      .createQueryBuilder("u")
-      .where("u.id = :id", { id: req.user.uid })
+      .getRepository('User')
+      .createQueryBuilder('u')
+      .where('u.id = :id', { id: req.user.uid })
       .getOne();
 
     await addLog(
@@ -292,16 +287,15 @@ router.delete("/:id", async (req, res) => {
       req.moduleName,
       `${user.names} ${user.lastnames}`,
       user.id,
-      `Se elimino la condicion de pago con nombre: ${paymentCondition.name}.`
+      `Se elimino la condicion de pago con nombre: ${paymentCondition.name}.`,
     );
 
     return res.json({
-      message: "La condicion de pago ha sido eliminada correctamente.",
+      message: 'La condicion de pago ha sido eliminada correctamente.',
     });
   } catch (error) {
     return res.status(500).json({
-      message:
-        "Error al eliminar la condicion de pago. Conctacta a tu administrador.",
+      message: 'Error al eliminar la condicion de pago. Conctacta a tu administrador.',
     });
   }
 });
