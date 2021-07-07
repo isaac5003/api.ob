0f74dd6f9f3daadb5f959e8150d7d62851620adf
Bryanavalos-dev <brayanavalos91@gmail.com>
2021-07-07 16:16:52 -0600

    OB-433: ADVANCES  -se creo el metodo getCompaniesWithIntegrations

--- src/auth/repositories/Access.repository.ts
+++ src/auth/repositories/Access.repository.ts
@@ -1,3 +1,6 @@
+import { InternalServerErrorException } from '@nestjs/common';
+import { Company } from 'src/companies/entities/Company.entity';
+import { Module } from 'src/system/entities/Module.entity';
 import { EntityRepository, Repository } from 'typeorm';
 import { Access } from '../entities/Access.entity';
 
@@ -9,5 +12,22 @@ export class AccessRepository extends Repository<Access> {
       .leftJoinAndSelect('a.branch', 'b')
       .where('a.profile = :profile', { profile })
       .getOne();
+  }
+
+  async getCompaniesWithIntegrations(receiverModule: string, integratedModule: string): Promise<Company[]> {
+    let companiesWithIntegrations;
+
+    try {
+      companiesWithIntegrations = await this.createQueryBuilder('c')
+        .select(['co.id', 'co.name', 'co.shortName'])
+        .leftJoin('c.company', 'co')
+        .where('c.module =:module', { module: receiverModule })
+        .andWhere('c.module=:module', { module: integratedModule })
+        .andWhere('co.active =:active', { active: true })
+        .getMany();
+    } catch (error) {
+      throw new InternalServerErrorException('Error al obtener el listado de empresas.');
+    }
+    return companiesWithIntegrations;
   }
 }
